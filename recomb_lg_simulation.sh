fe384f5483e9a0772c913baee4a8fe0a97eeb8e9
ybao2 <57085049+ybao2@users.noreply.github.com>
2020-04-09 15:56:48 +0800

    Update and rename recomb_lg_test.sh to recomb_lg_simulation.sh

--- recomb_lg_simulation.sh
+++ recomb_lg_simulation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 dir=$(pwd)
 scan_loop=100
 max_dist=4000
 min_num=2
+
 echo
 while [ -n "$1" ]
 do
@@ -51,9 +53,11 @@ do
 	esac
 	shift
 done
+
 echo "the number of scanning times the program will perform for the SNPs is $scan_loop"
 echo "maximum inter-SNP distance allowed within a cluster is $max_dist"
 echo "minimum number of SNPs per cluster is $min_num"
+
 mkdir $dir/snp_cluster_result
 #
 count=1
@@ -61,14 +65,18 @@ for param in "$@"
 do
 	echo "Parameter #$count: $param"
 	count=$[ $count + 1 ] 
-done	
+done
+
 length=$(($end_num-$start_num))
 echo $length
+
 number=$(($length/$step_size))
 echo "the toatl number of recombination length is $number "
+
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
 echo "Start running SNP clustering in parallel"
 echo $(seq $start_num $step_size $end_num)
+
 func()
 {
 	python MakeTest.py cluster -vcf $2 -anno $3 -operon $4 -recomb_lg $1 -out $5/snp_cluster_result/clust_out_$1  -scan_loop $6 -max_dist $7 -min_num $8
@@ -77,15 +85,18 @@ export -f func
 
 parallel -j 8 func ::: $(seq $start_num $step_size $end_num) ::: $vcf_file ::: $anno_file ::: $operon_file ::: $dir ::: $scan_loop ::: $max_dist ::: $min_num
 echo "The results of the clustering are saved in the snp_cluster_result"
+
 grep "recomb_lg" log.txt | awk -F "=" '{print $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' > recomb.txt
 rm log.txt
 echo "The result of the relationship between recombination length and the cluster number are saved in recomb_result.txt"
+
 echo "Start to run R program and use generalized additive model and linear model to fit the data"
 Rscript ./library/recomb_lg_Rscript $interv
 rm recomb.txt
 echo "The description of the R program is stored in Rcode_log.txt"
 echo "The results of the R program run are saved in Rplots.pdf"
+
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date="$starttime" +%s)
 end_seconds=$(date --date="$endtime" +%s)
-echo "本次运行时间： "$((end_seconds-start_seconds))"s"
+echo "The total time used is:： "$((end_seconds-start_seconds))"s"
