a0a1c5f6e3e441b9e6bffa64d7fbba8cba6366c1
ybao2 <57085049+ybao2@users.noreply.github.com>
2020-06-29 23:19:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="SweepCluster",
+    version='1.2',
+    description="Selective sweep detection method based on SNP clustering.",
+    classifiers=['License :: GPL-3.0','Operating System :: POSIX :: Linux'],
+    author="Junhui Qiu and Yun-Juan Bao",
+    url="https://github.com/BaoCodeLab/SweepCluster",
+    packages=find_packages(),
+    python_requires=">3.7.0",
+    install_requires=["numpy","scipy","pandas","PyVCF","scikit-learn"]
